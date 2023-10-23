0e1c3d7eb6b568dad6e35aab2e15b68a52ec5a25
turnup17 <jimjimkeke123@gmail.com>
2023-10-23 23:41:42 +0900

    changed env var settings

--- hackathon/dao/user_dao.go
+++ hackathon/dao/user_dao.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/joho/godotenv"
 	"log"
 	"os"
 	"os/signal"
@@ -15,15 +14,18 @@ var Db *sql.DB
 
 func init() {
 	// ①-1
-	if err := godotenv.Load(".env_mysql"); err != nil {
-		log.Fatalf("Error loading .env_mysql file: %v\n", err)
-	}
+	/*
+		if err := godotenv.Load(".env_mysql"); err != nil {
+			log.Fatalf("Error loading .env_mysql file: %v\n", err)
+		}
 
+	*/
 	// Get MySQL connection details from environment variables
 	mysqlUser := os.Getenv("MYSQL_USER")
 	mysqlUserPwd := os.Getenv("MYSQL_PASSWORD")
 	mysqlHost := os.Getenv("MYSQL_HOST")
 	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
+	fmt.Printf("Yo %s\n", mysqlUser)
 	// ①-2
 	_db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlUserPwd, mysqlHost, mysqlDatabase))
 	if err != nil {
