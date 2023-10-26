ef45d572fb7d38e311047e632a38132828819c98
turnup17 <jimjimkeke123@gmail.com>
2023-10-27 00:07:18 +0900

    changed url again

--- hackathon/main.go
+++ hackathon/main.go
@@ -14,7 +14,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if origin == "http://localhost:3000" || origin == "localhost:3000" {
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 	} else {
-		w.Header().Set("Access-Control-Allow-Origin", "uttc-hackathon-web.vercel.app")
+		w.Header().Set("Access-Control-Allow-Origin", "https://uttc-hackathon-web.vercel.app")
 	}
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST") // Specify the allowed methods
 
